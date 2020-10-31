48191dbb44b85a46e337d7a822331040e92aa142
crystalcrandall92 <67281675+crystalcrandall92@users.noreply.github.com>
2020-10-31 00:25:33 -0400

    Update isAuthenticated.js

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,11 +1,18 @@
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
 module.exports = function(req, res, next) {
     // If the user is logged in, continue with the request to the restricted route
+    console.log(req.cookies)
     if (req.user) {
       return next();
     }
-  
-    // If the user isn't logged in, redirect them to the login page
-    return res.redirect("/");
+
+    const cookie = req.cookies && req.cookies.someonescookie;
+
+    if (cookie) {
+      req.user = cookie
+      return next();
+    } else {
+      return res.redirect('/');
+    }
   };
   
