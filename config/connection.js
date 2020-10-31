33fb8eb3af77bd5b73218b300cf259006e27f26a
crystalcrandall92 <67281675+crystalcrandall92@users.noreply.github.com>
2020-10-31 02:38:08 -0400

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require('dotenv').config();
+
+let connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "books"
+
+});
+}
+// Make connection.
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
